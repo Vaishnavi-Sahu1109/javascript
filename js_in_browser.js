943872103fe901b3801c62dccc0321641fa03444
Vaishnavi Sahu <sahuvaishnavi1109@gmail.com>
2023-10-31 16:39:46 +0530

    first commi

---
+++ js_in_browser.js
@@ -0,0 +1,40 @@
+// question Node. 1
+// let age = 15
+// age = Number.parseInt(age)
+
+// const canDrive = (age)=>{
+//     return age>=18? true:false
+// }
+// if (canDrive(age)){
+//     console.log("yes you can drive")
+
+// }
+
+// else{
+//     console.log("you cannot drive")
+// }
+
+
+// question no 2
+
+// let age = 93
+// age = Number.parseInt(age)
+// if (age < 0 ){
+//     console.error("please enter the valid age");
+  
+// }
+// else {
+//     console.log("valid age")
+// }
+
+// question no 3
+
+// let number = 5
+// number = Number.parseInt(number)
+
+// if (number> 4 ){
+//     location.href ="https://google.com"
+
+// }
+
+
